ec7d3dbf9faaeeb579a4142f74f019b9f3c18d89
moondev03 <moondev03@gmail.com>
2024-08-31 17:36:38 +0900

    🛠️ Delete Project

--- Weave/app/src/main/java/com/studentcenter/weave/WeaveApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.studentcenter.weave
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class WeaveApplication : Application() {
-
-}
--- Weave/core/src/main/java/com/studentcenter/core/model/NetworkResult.kt
+++
@@ -1,9 +0,0 @@
-package com.studentcenter.core.model
-
-sealed interface NetworkResult<out T> {
-    data object Loading: NetworkResult<Nothing>
-
-    data class Success<T>(val data: T): NetworkResult<T>
-
-    data class Error(val error: NetworkError): NetworkResult<Nothing>
-}
