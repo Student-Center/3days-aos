6bc961981321d17b01bccac6507a6af20a216c53
moondev03 <moondev03@gmail.com>
2024-09-01 21:38:15 +0900

    ⚙️ SigningConfig Setting | Release

--- 3days/app/build.gradle.kts
+++ 3days/app/build.gradle.kts
@@ -48,6 +48,7 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
