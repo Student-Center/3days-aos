14bb4482d9b3cf18e45ef8aa4778ab57553987ea
moondev03 <moondev03@gmail.com>
2024-09-11 12:11:19 +0900

    🛠️ Setup Theme | Update ColorScheme

--- 3days/core/design-system/src/main/java/com/weave/design_system/ui/theme/Theme.kt
+++ 3days/core/design-system/src/main/java/com/weave/design_system/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.weave.a3days.ui.theme
+package com.weave.design_system.ui.theme
 
 import android.app.Activity
 import android.os.Build
@@ -9,34 +9,29 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    // 추후 Primary 색상 수정 필요
+    primary = DaysColor.Blue300,
+    secondary = DaysColor.Blue300,
+    tertiary = DaysColor.Blue300
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    // 추후 Primary 색상 수정 필요
+    primary = DaysColor.Blue300,
+    secondary = DaysColor.Blue300,
+    tertiary = DaysColor.Blue300
 )
 
 @Composable
 fun _3daysTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
