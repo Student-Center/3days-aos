67d13ccd399b0453d0e4696eea24195f6e99afbf
moondev03 <moondev03@gmail.com>
2024-10-01 22:24:22 +0900

    Update Navigation

--- 3days/feat/intro/src/main/java/com/weave/intro/IntroNavGraph.kt
+++ 3days/feat/intro/src/main/java/com/weave/intro/IntroNavGraph.kt
@@ -2,12 +2,34 @@ package com.weave.intro
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
 
 fun NavGraphBuilder.navGraphIntro(navController: NavController) {
     navigation(startDestination = "welcome", route = "intro") {
-        composable("welcome") { IntroScreen(navController) }
-        composable("mobile_auth") { MobileAuthScreen() }
+        composable("welcome") {
+            IntroScreen(
+                onClicked = { navController.navigate("mobile_send_auth") }
+            )
+        }
+        composable("mobile_send_auth") {
+            MobileSendAuthScreen(
+                onBackBtnClicked = { navController.popBackStack() },
+                onNextBtnClicked = { navController.navigate("mobile_enter_auth/$it") }
+            )
+        }
+        composable(
+            route = "mobile_enter_auth/{mobileNum}",
+            arguments = listOf(navArgument("mobileNum") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val mobileNum = backStackEntry.arguments?.getString("mobileNum") ?: ""
+            MobileEnterAuthScreen(
+                mobileNum = mobileNum,
+                onBackBtnClicked = { navController.popBackStack() },
+                onNextBtnClicked = { navController.navigate("") }
+            )
+        }
     }
 }
