c0b9b011192cbddb5d061b9a782fd55361501572
moondev03 <moondev03@gmail.com>
2024-10-02 14:54:55 +0900

    Optimizer Imports

--- 3days/core/design-system/src/main/java/com/weave/design_system/component/DaysGenderSelector.kt
+++ 3days/core/design-system/src/main/java/com/weave/design_system/component/DaysGenderSelector.kt
@@ -3,14 +3,24 @@ package com.weave.design_system.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -70,7 +80,12 @@ fun DaysGenderToggleButton(
         modifier = modifier
             .size(130.dp)
             .background(
-                brush = Brush.verticalGradient(if (isChecked) colors.first else listOf(colors.second, colors.second)),
+                brush = Brush.verticalGradient(
+                    if (isChecked) colors.first else listOf(
+                        colors.second,
+                        colors.second
+                    )
+                ),
                 shape = RoundedCornerShape(40.dp)
             )
             .border(
@@ -106,10 +121,8 @@ fun DaysGenderToggleButton(
 fun DaysGenderSelectorPreview() {
     var genderState by remember { mutableStateOf("") }
 
-    Scaffold(modifier = Modifier.fillMaxSize()) {
-        DaysGenderSelector(
-            genderState = genderState,
-            onChangedGender = { genderState = it }
-        )
-    }
+    DaysGenderSelector(
+        genderState = genderState,
+        onChangedGender = { genderState = it }
+    )
 }
