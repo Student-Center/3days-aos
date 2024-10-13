0e630b014fc9eda39907f4158406dd10b95cb95a
moondev03 <moondev03@gmail.com>
2024-10-14 00:40:07 +0900

    Add Auth Mapper

--- 3days/data/src/main/java/com/weave/data/mapper/AuthMapper.kt
+++ 3days/data/src/main/java/com/weave/data/mapper/AuthMapper.kt
@@ -1,11 +1,21 @@
 package com.weave.data.mapper
 
+import com.weave.model.auth.AuthRegisterToken
+import com.weave.model.auth.AuthToken
+import com.weave.model.auth.AuthVerifyToken
+import com.weave.network.model.ExistingUserVerifyCodeResponse
+import com.weave.network.model.NewUserVerifyCodeResponse
+import com.weave.network.model.SendAuthCodeResponse
+import com.weave.network.model.TokenResponse
 
-//val TokenResponse.toDomainModel
-//    get() = AuthToken(accessToken = accessToken, refreshToken = refreshToken)
-//
-//val AuthVerifyPost200Response.toDomainModel
-//    get() = AuthVerifyToken(accessToken = accessToken, refreshToken = refreshToken, registerToken = registerToken)
-//
-//val RegisterTokenResponse.toDomainModel
-//    get() = AuthRegisterToken(registerToken = registerToken)
+val ExistingUserVerifyCodeResponse.toDomain
+    get() = AuthToken(accessToken = accessToken, refreshToken = refreshToken)
+
+val NewUserVerifyCodeResponse.toDomain
+    get() = AuthRegisterToken(registerToken = registerToken, expiresIn = expiresIn ?: 0)
+
+val TokenResponse.toDomain
+    get() = AuthToken(accessToken = accessToken, refreshToken = refreshToken)
+
+val SendAuthCodeResponse.toDomain
+    get() = AuthVerifyToken(authCodeId = authCodeId, userStatus = userStatus.toString())
