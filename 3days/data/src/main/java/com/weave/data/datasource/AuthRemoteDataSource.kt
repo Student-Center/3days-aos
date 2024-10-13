39a84ec5f36fa9785182c28b75c2b90749b8dcc9
moondev03 <moondev03@gmail.com>
2024-10-14 00:41:31 +0900

    Add Auth APIs | Repository & DataSource

--- 3days/data/src/main/java/com/weave/data/datasource/AuthRemoteDataSource.kt
+++ 3days/data/src/main/java/com/weave/data/datasource/AuthRemoteDataSource.kt
@@ -1,12 +1,22 @@
 package com.weave.data.datasource
 
+import com.weave.model.network.NetworkResult
+import com.weave.network.model.ExistingUserVerifyCodeResponse
+import com.weave.network.model.NewUserVerifyCodeResponse
+import com.weave.network.model.RefreshTokenRequest
+import com.weave.network.model.SendAuthCodeRequest
+import com.weave.network.model.SendAuthCodeResponse
+import com.weave.network.model.TokenResponse
+import com.weave.network.model.VerifyCodeRequest
+import java.util.UUID
+
 interface AuthRemoteDataSource {
 
-//    suspend fun authPhonePost(phoneRequest: PhoneRequest): NetworkResult<Unit>
-//
-//    suspend fun authRefreshPost(refreshRequest: RefreshRequest): NetworkResult<TokenResponse>
-//
-//    suspend fun authRegisterPost(registerRequest: RegisterRequest): NetworkResult<TokenResponse>
-//
-//    suspend fun authVerifyPost(verificationRequest: VerificationRequest): NetworkResult<AuthVerifyPost200Response>
+    suspend fun existingUserVerifyCode(authCodeId: UUID, verifyCodeRequest: VerifyCodeRequest): NetworkResult<ExistingUserVerifyCodeResponse>
+
+    suspend fun newUserVerifyCode(authCodeId: UUID, verifyCodeRequest: VerifyCodeRequest): NetworkResult<NewUserVerifyCodeResponse>
+
+    suspend fun refreshToken(refreshTokenRequest: RefreshTokenRequest): NetworkResult<TokenResponse>
+
+    suspend fun requestVerification(sendAuthCodeRequest: SendAuthCodeRequest): NetworkResult<SendAuthCodeResponse>
 }
--- 3days/data/src/main/java/com/weave/data/repository/AuthRepositoryImpl.kt
+++ 3days/data/src/main/java/com/weave/data/repository/AuthRepositoryImpl.kt
@@ -1,70 +1,74 @@
 package com.weave.data.repository
 
 import com.weave.data.datasource.AuthRemoteDataSource
+import com.weave.data.mapper.mapToDomain
+import com.weave.data.mapper.toDomain
+import com.weave.domain.repository.AuthRepository
+import com.weave.model.auth.AuthRegisterToken
+import com.weave.model.auth.AuthToken
+import com.weave.model.auth.AuthVerifyToken
+import com.weave.model.network.NetworkResult
+import com.weave.network.model.RefreshTokenRequest
+import com.weave.network.model.SendAuthCodeRequest
+import com.weave.network.model.VerifyCodeRequest
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.util.UUID
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val dataSource: AuthRemoteDataSource
-) {
+) : AuthRepository {
 
-//    override suspend fun authPhonePost(phoneNumber: String): Flow<NetworkResult<Unit>> = flow {
-//        emit(NetworkResult.Loading)
-//        Log.d("AuthRepositoryImpl", "authPhonePost called with $phoneNumber")
-//        when (val result = dataSource.authPhonePost(PhoneRequest(phoneNumber))) {
-//            is NetworkResult.Success -> {
-//                Log.d("AuthRepositoryImpl", "Success: ${result.data}")
-//                emit(NetworkResult.Success(result.data))
-//            }
-//            is NetworkResult.Error -> {
-//                Log.d("AuthRepositoryImpl", "Error: ${result.error}")
-//                emit(NetworkResult.Error(result.error))
-//            }
-//            NetworkResult.Loading -> {
-//                Log.d("AuthRepositoryImpl", "Loading")
-//            }
-//        }
-//    }
-//
-//    override suspend fun authRefreshPost(refreshToken: String): Flow<NetworkResult<AuthToken>> =
-//        flow {
-//            emit(NetworkResult.Loading)
-//            when (val result = dataSource.authRefreshPost(RefreshRequest(refreshToken))) {
-//                is NetworkResult.Success -> emit(NetworkResult.Success(result.data.toDomainModel))
-//                is NetworkResult.Error -> emit(NetworkResult.Error(result.error))
-//                NetworkResult.Loading -> {}
-//            }
-//        }
-//
-//    override suspend fun authRegisterPost(
-//        registerToken: String,
-//        name: String,
-//        email: String
-//    ): Flow<NetworkResult<AuthToken>> = flow {
-//        emit(NetworkResult.Loading)
-//        when (val result = dataSource.authRegisterPost(
-//            RegisterRequest(
-//                registerToken = registerToken,
-//                profile = Profile(name = name, email = email)
-//            )
-//        )) {
-//            is NetworkResult.Success -> emit(NetworkResult.Success(result.data.toDomainModel))
-//            is NetworkResult.Error -> emit(NetworkResult.Error(result.error))
-//            NetworkResult.Loading -> {}
-//        }
-//    }
-//
-//
-//    override suspend fun authVerifyPost(
-//        phoneNumber: String,
-//        code: String
-//    ): Flow<NetworkResult<AuthVerifyToken>> =
-//        flow {
-//            emit(NetworkResult.Loading)
-//            when (val result =
-//                dataSource.authVerifyPost(VerificationRequest(phone = phoneNumber, code = code))) {
-//                is NetworkResult.Success -> emit(NetworkResult.Success(result.data.toDomainModel))
-//                is NetworkResult.Error -> emit(NetworkResult.Error(result.error))
-//                NetworkResult.Loading -> {}
-//            }
-//        }
+    override suspend fun existingUserVerifyCode(
+        authCodeId: UUID,
+        verifyCode: String
+    ): Flow<NetworkResult<AuthToken>> = flow {
+        emit(NetworkResult.Loading)
+        when (val result = dataSource.existingUserVerifyCode(
+            authCodeId = authCodeId,
+            verifyCodeRequest = VerifyCodeRequest(verifyCode)
+        )) {
+            is NetworkResult.Success -> emit(NetworkResult.Success(result.data.toDomain))
+            is NetworkResult.Error -> emit(NetworkResult.Error(result.error))
+            NetworkResult.Loading -> {}
+        }
+    }
+
+    override suspend fun newUserVerifyCode(
+        authCodeId: UUID,
+        verifyCode: String
+    ): Flow<NetworkResult<AuthRegisterToken>> = flow {
+        emit(NetworkResult.Loading)
+        when (val result = dataSource.newUserVerifyCode(
+            authCodeId = authCodeId,
+            verifyCodeRequest = VerifyCodeRequest(verifyCode)
+        )) {
+            is NetworkResult.Success -> emit(NetworkResult.Success(result.data.toDomain))
+            is NetworkResult.Error -> emit(NetworkResult.Error(result.error))
+            NetworkResult.Loading -> {}
+        }
+    }
+
+    override suspend fun refreshToken(): Flow<NetworkResult<AuthToken>> = flow {
+        emit(NetworkResult.Loading)
+        when (val result = dataSource.refreshToken(
+            RefreshTokenRequest("todo: JWT DataStore 추가 후 반영")
+        )) {
+            is NetworkResult.Success -> emit(NetworkResult.Success(result.data.toDomain))
+            is NetworkResult.Error -> emit(NetworkResult.Error(result.error))
+            NetworkResult.Loading -> {}
+        }
+    }
+
+    override suspend fun requestVerification(phoneNumber: String): Flow<NetworkResult<AuthVerifyToken>> = flow {
+        emit(NetworkResult.Loading)
+        when (val result = dataSource.requestVerification(
+            sendAuthCodeRequest = SendAuthCodeRequest(phoneNumber = phoneNumber)
+        )) {
+            is NetworkResult.Success -> emit(NetworkResult.Success(result.data.toDomain))
+            is NetworkResult.Error -> emit(NetworkResult.Error(result.error))
+            NetworkResult.Loading -> {}
+        }
+    }
 }
