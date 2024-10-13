ded6ff1250e06f0f22d37625fabe38bddb275c20
moondev03 <moondev03@gmail.com>
2024-10-13 20:01:16 +0900

    Fix DaysOnlyBackAppbar Height

--- 3days/core/design-system/src/main/java/com/weave/design_system/component/DaysOnlyBackAppbar.kt
+++ 3days/core/design-system/src/main/java/com/weave/design_system/component/DaysOnlyBackAppbar.kt
@@ -1,9 +1,12 @@
 package com.weave.design_system.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -15,6 +18,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,24 +35,20 @@ fun DaysOnlyBackAppbar(
     TopAppBar(
         modifier = Modifier
             .fillMaxWidth()
-            .height(54.dp),
+            .heightIn(min = 54.dp),
         title = { Text(text = "") },
         navigationIcon = {
-            Box(modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.CenterStart)
-            {
-                Box(
-                    modifier = Modifier
-                        .size(48.dp, 48.dp)
-                        .padding(start = 6.dp, top = 8.dp)
-                        .noRippleClickable(onBackPressed),
-                ) {
-                    Icon(
-                        modifier = Modifier.size(24.dp, 24.dp),
-                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_round_arrow_back),
-                        contentDescription = "Back"
-                    )
-                }
+            Box(
+                modifier = Modifier
+                    .size(48.dp, 48.dp)
+                    .noRippleClickable(onBackPressed),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    modifier = Modifier.size(24.dp, 24.dp),
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_round_arrow_back),
+                    contentDescription = "Back"
+                )
             }
         },
         colors = TopAppBarDefaults.topAppBarColors().copy(
