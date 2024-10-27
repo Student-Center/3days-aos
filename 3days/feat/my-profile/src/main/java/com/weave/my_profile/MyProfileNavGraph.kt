6ec4aac50df6f3794e70c2190bdd6af5c80f6c40
moondev03 <moondev03@gmail.com>
2024-10-27 21:33:29 +0900

    Refactor NavGraphs

--- 3days/feat/my-profile/src/main/java/com/weave/my_profile/MyProfileNavGraph.kt
+++ 3days/feat/my-profile/src/main/java/com/weave/my_profile/MyProfileNavGraph.kt
@@ -12,26 +12,50 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import com.weave.my_profile.birth.MyProfileBirthYearScreen
+import com.weave.my_profile.company.MyProfileCompanyScreen
 import com.weave.my_profile.gender.MyProfileGenderScreen
 import com.weave.my_profile.init.MyProfileInitScreen
 
+enum class Route(val routeName: String) {
+    MyProfile("my_profile"),
+    MyProfileInit("my_profile_init"),
+    MyProfileGender("my_profile_gender"),
+    MyProfileBirth("my_profile_birth"),
+    MyProfileCompany("my_profile_company"),
+    NextScreen("next_screen");
+
+    fun withArgs(vararg args: String): String {
+        return buildString {
+            append(routeName)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
+}
+
 fun NavGraphBuilder.navGraphMyProfile(navController: NavController) {
     navigation(
-        startDestination = "my_profile_init/{registerToken}",
-        route = "my_profile/{registerToken}"
+        startDestination = Route.MyProfileInit.withArgs("{registerToken}"),
+        route = Route.MyProfile.withArgs("{registerToken}")
     ) {
         composable(
-            route = "my_profile_init/{registerToken}",
+            route = Route.MyProfileInit.withArgs("{registerToken}"),
             arguments = listOf(navArgument("registerToken") { type = NavType.StringType })
         ) { backStackEntry ->
             val registerToken = backStackEntry.arguments?.getString("registerToken") ?: ""
 
             MyProfileInitScreen(
-                onNextBtnClicked = { navController.navigate("my_profile_gender/$registerToken") },
+                onNextBtnClicked = {
+                    navController.navigateWithClearBackStack(
+                        Route.MyProfileGender.withArgs(registerToken),
+                        Route.MyProfile.routeName
+                    )
+                },
             )
         }
         composable(
-            route = "my_profile_gender/{registerToken}",
+            route = Route.MyProfileGender.withArgs("{registerToken}"),
             arguments = listOf(navArgument("registerToken") { type = NavType.StringType })
         ) { backStackEntry ->
             val registerToken = backStackEntry.arguments?.getString("registerToken") ?: ""
@@ -42,19 +66,53 @@ fun NavGraphBuilder.navGraphMyProfile(navController: NavController) {
             MyProfileGenderScreen(
                 sharedViewModel = sharedViewModel,
                 onBackBtnClicked = { navController.popBackStack() },
-                onNextBtnClicked = { navController.navigate("my_profile_birth") }
+                onNextBtnClicked = {
+                    navController.navigateWithClearBackStack(
+                        Route.MyProfileBirth.routeName,
+                        Route.MyProfileInit.routeName
+                    )
+                }
             )
         }
-        composable("my_profile_birth") {
+        composable(Route.MyProfileBirth.routeName) {
             val sharedViewModel =
                 it.sharedViewModel<MyProfileSharedViewModel>(navController = navController)
 
             MyProfileBirthYearScreen(
                 sharedViewModel = sharedViewModel,
                 onBackBtnClicked = { navController.popBackStack() },
-                onNextBtnClicked = { navController.navigate("next_screen") }
+                onNextBtnClicked = {
+                    navController.navigateWithClearBackStack(
+                        Route.MyProfileCompany.routeName,
+                        Route.MyProfileGender.routeName
+                    )
+                }
             )
         }
+        composable(Route.MyProfileCompany.routeName) {
+            val sharedViewModel =
+                it.sharedViewModel<MyProfileSharedViewModel>(navController = navController)
+
+            MyProfileCompanyScreen(
+                sharedViewModel = sharedViewModel,
+                onBackBtnClicked = { navController.popBackStack() },
+                onNextBtnClicked = {
+                    navController.navigateWithClearBackStack(
+                        Route.NextScreen.routeName,
+                        Route.MyProfileBirth.routeName
+                    )
+                }
+            )
+        }
+    }
+}
+
+
+private fun NavController.navigateWithClearBackStack(
+    destination: String, popUpToRoute: String
+) {
+    this.navigate(destination) {
+        popUpTo(popUpToRoute)
     }
 }
 
