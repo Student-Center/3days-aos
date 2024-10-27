341d3a1fba385536ca35a01f25bc5f13fb7f5835
moondev03 <moondev03@gmail.com>
2024-10-27 21:20:36 +0900

    Add handleNetworkCall

---
+++ 3days/data/src/main/java/com/weave/data/extension/HandleNetworkCall.kt
@@ -0,0 +1,17 @@
+package com.weave.data.extension
+
+import com.weave.model.network.NetworkResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+fun <T, R> handleNetworkCall(
+    networkCall: suspend () -> NetworkResult<T>,
+    mapToDomain: (T) -> R
+): Flow<NetworkResult<R>> = flow {
+    emit(NetworkResult.Loading)
+    when (val result = networkCall()) {
+        is NetworkResult.Success -> emit(NetworkResult.Success(mapToDomain(result.data)))
+        is NetworkResult.Error -> emit(NetworkResult.Error(result.error))
+        NetworkResult.Loading -> {}
+    }
+}
