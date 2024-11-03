8530bacc0d0536b279d9e3f546a8c2cc4727df29
moondev03 <moondev03@gmail.com>
2024-11-03 16:11:54 +0900

    Add Dependency Module | :domain:location

--- 3days/feat/my-profile/build.gradle.kts
+++ 3days/feat/my-profile/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(project(":core:utils"))
     implementation(project(":core:design-system"))
     implementation(project(":domain:company"))
+    implementation(project(":domain:location"))
 
     implementation(libs.bundles.compose)
     debugImplementation(libs.bundles.compose.debug)
