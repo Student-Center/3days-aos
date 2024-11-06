e3df7757b695739e98345f6390778699c0214f5a
moondev03 <moondev03@gmail.com>
2024-11-06 22:26:03 +0900

    Add UseCase

--- 3days/domain/user/src/main/java/com/weave/user/RegisterUserUseCase.kt
+++ 3days/domain/user/src/main/java/com/weave/user/RegisterUserUseCase.kt
@@ -2,8 +2,7 @@ package com.weave.user
 
 import com.weave.domain.repository.UserRepository
 import com.weave.model.auth.AuthToken
-import com.weave.model.domain.user.UserDesiredPartner
-import com.weave.model.domain.user.UserProfile
+import com.weave.model.domain.user.RegisterInfo
 import com.weave.model.network.NetworkResult
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -13,16 +12,10 @@ class RegisterUserUseCase @Inject constructor(
 ) {
     suspend operator fun invoke(
         xRegisterToken: String,
-        name: String,
-        phoneNumber: String,
-        profile: UserProfile,
-        desiredPartner: UserDesiredPartner
+        registerInfo: RegisterInfo
     ): Flow<NetworkResult<AuthToken>> =
         repository.registerUser(
             xRegisterToken = xRegisterToken,
-            name = name,
-            phoneNumber = phoneNumber,
-            profile = profile,
-            desiredPartner = desiredPartner
+            registerInfo = registerInfo
         )
 }
