055c39e5d2b7f19b9f95e277d1696fa290dbfdae
moondev03 <moondev03@gmail.com>
2024-11-17 01:28:16 +0900

    Apply Client Module | AnnotationRetention.Binary

--- 3days/data/src/main/java/com/weave/data/di/ClientModule.kt
+++ 3days/data/src/main/java/com/weave/data/di/ClientModule.kt
@@ -1,18 +1,31 @@
 package com.weave.data.di
 
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
+import android.content.Context
 import com.weave.data.BuildConfig
+import com.weave.data.utils.AuthInterceptor
 import com.weave.data.utils.PrettyJsonLogger
+import com.weave.domain.repository.TokenRepository
+import com.weave.network.api.AuthApi
+import com.weave.network.infrastructure.Serializer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Qualifier
 import javax.inject.Singleton
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class AuthorizedClient
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class UnauthorizedClient
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -20,21 +33,26 @@ class ClientModule {
 
     @Provides
     @Singleton
-    fun provideGson(): Gson {
-        return GsonBuilder().create()
-    }
-
-    @Provides
-    @Singleton
     fun provideLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor(
         PrettyJsonLogger()
     ).apply {
-        if(BuildConfig.DEBUG) setLevel(HttpLoggingInterceptor.Level.BODY)
+        if (BuildConfig.DEBUG) setLevel(HttpLoggingInterceptor.Level.BODY)
         else setLevel(HttpLoggingInterceptor.Level.NONE)
     }
 
     @Provides
     @Singleton
+    fun provideAuthInterceptor(
+        @ApplicationContext context: Context,
+        tokenRepository: TokenRepository,
+        authApi: AuthApi
+    ): AuthInterceptor {
+        return AuthInterceptor(context, authApi, tokenRepository)
+    }
+
+    @Provides
+    @Singleton
+    @UnauthorizedClient
     fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
@@ -43,11 +61,40 @@ class ClientModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit {
+    @UnauthorizedClient
+    fun provideRetrofit(
+        @UnauthorizedClient okHttpClient: OkHttpClient,
+    ): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addConverterFactory(GsonConverterFactory.create(Serializer.gson))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @AuthorizedClient
+    fun provideOkHttpClientWithToken(
+        loggingInterceptor: HttpLoggingInterceptor,
+        authInterceptor: AuthInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @AuthorizedClient
+    fun provideRetrofitWithToken(
+        @AuthorizedClient okHttpClient: OkHttpClient,
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(Serializer.gson))
             .build()
     }
 }
