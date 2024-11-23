78fae730cd931f2e5634b9a63b7d7f4b1cecfb62
moondev03 <moondev03@gmail.com>
2024-11-23 21:56:46 +0900

    Add Common SnackBarViewModel

---
+++ 3days/feat/home/src/main/java/com/weave/home/profile/SnackBarViewModel.kt
@@ -0,0 +1,27 @@
+package com.weave.home.profile
+
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.weave.design_system.component.SnackBarType
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SnackBarViewModel : ViewModel() {
+    val snackBarHostState = SnackbarHostState()
+
+    fun showSnackBar(message: String, type: SnackBarType) {
+        viewModelScope.launch {
+            val job = launch {
+                snackBarHostState.showSnackbar(
+                    message = message,
+                    actionLabel = type.toString(),
+                    duration = SnackbarDuration.Indefinite
+                )
+            }
+            delay(3000L)
+            job.cancel()
+        }
+    }
+}
